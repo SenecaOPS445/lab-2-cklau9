7fd475672f60ce6cbde502046d0ad7813e16a1d9
Chikong LAU <cklau9@myseneca.ca>
2024-05-17 13:04:25 -0400

    Added lab2b

---
+++ lab2b.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# name = 'Jon'
+# age = 20
+
+name = input("Name: ")
+age = int(input("Age: "))
+
+
+# print("Name: " + name)
+# print("Age: " + str(age))
+print("Hi " + name + "," + " you are " + str(age) + " years old.")
