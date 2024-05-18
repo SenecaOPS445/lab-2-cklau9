4703b215144f0afc4bf119b96b88370d4296519b
Chikong LAU <cklau9@myseneca.ca>
2024-05-18 14:31:54 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+Author:  LAU Chi Kong
+Author ID:  cklau9
+Date Created:  2024/05/17
+
+'''
+
+timer = 10 #Startup value from 10
+while timer != 0: #Set the WHILE loop stop at 0
+    print(timer)
+    timer = timer - 1
+print('blast off!')
